17b8aba567388e40882884a263d88fa562666afb
rodeyna <ahmidrodeyna@gmail.com>
2025-02-18 19:10:34 +0100

    centering the contariner

--- src/componant/HtmlTagSelector.jsx
+++ src/componant/HtmlTagSelector.jsx
@@ -9,8 +9,17 @@ const HtmlTagSelector = () => {
   const [url, setUrl] = useState(""); // Champ pour l'URL
 
   const htmlTags = [
-    "<h1>", "<section>", "<div>", "<p>", "<ul>", "<li>", "<footer>",
-    "<header>", "<article>", "<span>", "<nav>", "<aside>", "<main>"
+   "<a>", "<abbr>", "<address>", "<area>", "<article>", "<aside>", "<audio>", "<b>", "<base>", "<bdi>", "<bdo>", 
+"<blockquote>", "<body>", "<br>", "<button>", "<canvas>", "<caption>", "<cite>", "<code>", "<col>", "<colgroup>", 
+"<data>", "<datalist>", "<dd>", "<del>", "<details>", "<dfn>", "<dialog>", "<div>", "<dl>", "<dt>", "<em>", "<embed>",
+"<fieldset>", "<figcaption>", "<figure>", "<footer>", "<form>", "<h1>", "<h2>", "<h3>", "<h4>", "<h5>", "<h6>",
+"<head>", "<header>", "<hgroup>", "<hr>", "<html>", "<i>", "<iframe>", "<img>", "<input>", "<ins>", "<kbd>", "<label>",
+"<legend>", "<li>", "<link>", "<main>", "<map>", "<mark>", "<meta>", "<meter>", "<nav>", "<noscript>", "<object>",
+"<ol>", "<optgroup>", "<option>", "<output>", "<p>", "<param>", "<picture>", "<pre>", "<progress>", "<q>", "<rp>", 
+"<rt>", "<ruby>", "<s>", "<samp>", "<script>", "<section>", "<select>", "<small>", "<source>", "<span>", "<strong>",
+"<style>", "<sub>", "<summary>", "<sup>", "<table>", "<tbody>", "<td>", "<template>", "<textarea>", "<tfoot>",
+"<th>", "<thead>", "<time>", "<title>", "<tr>", "<track>", "<u>", "<ul>", "<var>", "<video>", "<wbr>"
+
   ];
 
   const filteredTags = htmlTags.filter((tag) =>
